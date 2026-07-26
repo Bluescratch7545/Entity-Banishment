2e9cac7d5e34cf8c856039d7f921cfdf1859264c
Bluescratch <dzadu02@gmail.com>
2026-07-26 13:27:55 +0200

    aaaaaa

---
+++ src/main/java/com/bluescratch/entitybanishment/block/BanishmentBlock.java
@@ -0,0 +1,26 @@
+package com.bluescratch.entitybanishment.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+public class BanishmentBlock extends Block {
+    public BanishmentBlock(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
+        if (!level.isClientSide) {
+            player.sendSystemMessage(Component.literal("The banishment block has been activated!"));
+        }
+
+        return InteractionResult.SUCCESS;
+    }
+}
--- src/main/java/com/bluescratch/entitybanishment/register/ModBlocks.java
+++ src/main/java/com/bluescratch/entitybanishment/register/ModBlocks.java
@@ -2,6 +2,7 @@ package com.bluescratch.entitybanishment.register;
 
 import com.bluescratch.entitybanishment.EntityBanishment;
 
+import com.bluescratch.entitybanishment.block.BanishmentBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -11,5 +12,11 @@ public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(EntityBanishment.MOD_ID);
 
     public static final DeferredBlock<Block> BANISHMENT_BLOCK =
-            BLOCKS.register("banishment_block", () -> new Block(BlockBehaviour.Properties.of()));
+            BLOCKS.register(
+                    "banishment_block",
+                    () -> new BanishmentBlock(
+                        BlockBehaviour.Properties.of()
+                                .lightLevel(state -> 8)
+                    )
+            );
 }
